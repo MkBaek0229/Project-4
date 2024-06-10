795c69d0e9973ffbc136c3f86eed37c0e0ddd2e2
mac <alsrl6678@naver.com>
2024-06-10 20:29:19 +0900

    리액트라우터도입

--- 1951026 백민기/src/App.jsx
+++ 1951026 백민기/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import "./App.css";
+import Home from "./page/Home";
+import Sub1 from "./page/Sub";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="/sub1" element={<Sub1 />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
