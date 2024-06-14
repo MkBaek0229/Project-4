a5bd264ac37f187857aec1e2b85a61bbb8975315
Minki Baek <alsrl6678@naver.com>
2024-06-14 17:32:57 +0900

    홈페이지 내용 구성

--- 1951026 백민기/src/pages/HomePage.jsx
+++ 1951026 백민기/src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
+import Home from "../components/Home";
+
 function HomePage() {
   return (
     <div className="flex">
-      <section className="flex-1">d</section>
-      <div className="flex-1 w-4 bg-black">
-        <section>s</section>
-        <section>s</section>
-      </div>
+      <Home />
     </div>
   );
 }
